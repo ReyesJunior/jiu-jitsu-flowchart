4c57b738576fe0d35004bfe758421291c72c1077
Jorge Reyes Jr <reyes.junior.27@gmail.com>
2015-10-27 12:14:38 -0500

    Added js code to flip the dropdown menu icons when the js-sub-section-headers are clicked

--- gi/gi-flowchart.js
+++ gi/gi-flowchart.js
@@ -8,20 +8,23 @@ $(document).ready(function () {
   });
 
 
-// Toggle SlideDOwn/Slideup Menu when gi-dropdown-menu-item is clicked
+// Toggle SlideDown/Slideup Menu when gi-dropdown-menu-item is clicked
   $('.gi-dropdown-menu-item').click( function() {
     $('.gi-dropdown-menu').animate({height: 'toggle'}, 250, function () {
     });
   });
 
 
-// Toggle Slider Menus
+// Toggle Slider Menus and Slider Menus icon when js-section-header is clicked
    $('.js-section-header').click( function() {
-      $(this).toggleClass( 'sub-section-header-clicked' );
+      $(this).toggleClass('sub-section-header-clicked');
 
     var target = $( this ).data('target');
       $('.slider-wrapper[data-id="' + target + '"]').slideToggle('display');
+      $('.toggle-scroll-paragraph-button[data-rotate="' + target + '"]').toggleClass('rotate');   
   });
+
+
 
 
 });
