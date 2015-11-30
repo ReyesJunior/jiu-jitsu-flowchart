237efb0610775a09e770ae65face3d6e0d4ad7ca
Jorge Reyes Jr <reyes.junior.27@gmail.com>
2015-11-29 18:32:35 -0600

    Changed 'gi-dropdown' to 'gi-mobile'

--- gi/gi-flowchart-homepage.js
+++ gi/gi-flowchart-homepage.js
@@ -3,15 +3,15 @@
 $(document).ready(function () {
 
 // Toggle SlideDown/Slideup Menu when clicking dropdown-menu-button
-  $('.dropdown-menu-button').click( function() {
-    $('.gi-dropdown-menu').animate({height: 'toggle'}, 250, function () {
+  $('.mobile-menu-button').click( function() {
+    $('.gi-mobile-menu').animate({height: 'toggle'}, 250, function () {
     });
   });
 
 
 // Toggle SlideDown/Slideup Menu when clicking gi-dropdown-menu-item
-  $('.gi-dropdown-menu-item').click( function() {
-    $('.gi-dropdown-menu').animate({height: 'toggle'}, 250, function () {
+  $('.gi-mobile-menu-item').click( function() {
+    $('.gi-mobile-menu').animate({height: 'toggle'}, 250, function () {
     });
   });
 
